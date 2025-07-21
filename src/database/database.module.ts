9aeaed0e362ce75f930e725d86d9803f212619d1
Carlos David Prado Hernandez <davidkelthaz@gmail.com>
2025-07-21 17:07:18 -0500

    Se cambia motor de BD de sql server a MySql

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,17 +7,13 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Habilita variables de entorno
     TypeOrmModule.forRoot({
-      type: 'mssql',
+      type: 'mysql',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT!, 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      // synchronize: true, // Solo en desarrollo (crea tablas automáticamente)
-      options: {
-        encrypt: false, // importante si tu SQL Server no requiere SSL
-      },
     }),
   ],
 })
